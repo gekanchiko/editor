c0aeb892c8062d146c379cc71796128ab70777d4
Evgenij Olshanskij <evgenij@selina.com>
2019-08-23 15:44:32 +0300

    Implement loadable module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+console.log('module2 loaded');
+
+const Comp = () => <div><p>MODULE 1</p></div>;
+
+const attach = () => {
+  window.module2 = () => {
+    delete window.module2;
+    return {
+      routes: [
+        {
+          exact: true,
+          path: '/mod1',
+          component: Comp
+        }
+      ],
+      navBarItems: [
+        { path: '/mod1', name: 'Module 1' }
+      ]
+    };
+  };
+};
+
+attach();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
